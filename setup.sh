b7677f6ecc55543bc1bf60690f133f2544dfd274
Rishab Jasrotia <rishabjasrotia@Rishabs-MacBook-Pro.local>
2024-02-16 10:14:02 +0530

    Fix the Dev docker setup local

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo chown -R 1000:33 "/var/www/.npm"
+
+printf "Installing Mautic Composer dependencies...\n"
+composer install
+cp ./local.config.php.dist ./config/local.php
+cp ./.env-example ./.env.test.local
+cp ./.env.local.dist ./.env.local
+
+printf "Installing Mautic...\n"
+./bin/console mautic:install "mautic-test.com"
+./bin/console cache:warmup --no-interaction --env=dev
